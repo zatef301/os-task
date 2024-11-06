434becb489823e4866625439c3ec2d6391df2e65
ziad atef <zatef301@gmail.com>
2024-11-06 18:07:10 +0200

    first

---
+++ Tese.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tese;
+
+/**
+ *
+ * @author zatef
+ */
+public class Tese {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
